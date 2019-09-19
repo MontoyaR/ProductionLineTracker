0e0e1b37a3d40e0f07b9858ca2d03084031c6683
ramontoya7474 <ramontoya7474@eagle.fgcu.edu>
2019-09-18 21:36:55 -0400

    Week 3 — New Addition

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,10 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -20,13 +25,57 @@ public class Main extends Application {
    */
   @Override
   public void start(Stage primaryStage) throws Exception {
+
+    initializeDB();
+
+
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("Hello World");
+    primaryStage.setTitle("Production Line Tracker");
     primaryStage.setScene(new Scene(root, 600, 400));
     primaryStage.show();
 
 
   }
+
+  public void initializeDB(){
+
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/HR";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+
+      String sql = "SELECT * FROM PRODUCT";
+
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        System.out.println(rs.getString(1));
+      }
+
+      // STEP 4: Clean-up environment
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
 
   public static void main(String[] args) {
     launch(args);

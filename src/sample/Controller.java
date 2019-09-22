2003a1ebb54b82b06812b6caf42741984f3aac83
ramontoya7474 <ramontoya7474@eagle.fgcu.edu>
2019-09-21 21:14:48 -0400

    Week 5 — Complete

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,20 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
 
-public class Controller {
+public class Controller implements Initializable {
 
   @FXML
   private Button btnPrint;
+
 
   @FXML
   void Println(MouseEvent event) {
@@ -16,6 +23,18 @@ public class Controller {
 
   @FXML
   void addProduct(MouseEvent event) {
+
     System.out.println("Printing to console.");
+  }
+
+  @FXML
+  public ComboBox<String> combobox;
+  ObservableList<String> list = FXCollections
+      .observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    combobox.setItems(list);
+
   }
 }
